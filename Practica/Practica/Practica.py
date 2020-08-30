33f6d7b8ccdd8dede20cc76695874a0a347a0c7e
Carlos Castro <2786954930101@ingenieria.usac.edu.gt>
2020-08-30 17:37:28 -0600

    en funcion SELECCIONAR ya encuentra datos de algun archivo JSON

--- Practica/Practica/Practica.py
+++ Practica/Practica/Practica.py
@@ -1,10 +1,12 @@
 import json
 import sys
-from io import open
+import re
 
-print('Bienvenido usa Algun Comando')
+print('Bienvenido usa Algun Comando --->  ')
+print('(CARGAR)-(SELECCIONAR)-(SELECCIONAR*)-(SALIR)')
 salir = False
 while salir==False:
+    global comando
     comando = input()     # lo que va ha ingresar el usuario
     comando = comando.replace(",","")   #remplazando comas por nada jeje
     global ListaTextoComando 
@@ -25,6 +27,28 @@ while salir==False:
             contador = contador+1
     print(len(listaArchivos))
 
+    if ListaTextoComando[0] == "SELECCIONAR":
+        if re.search("DONDE",comando):
+            patron = r'"(.*?)"'     #para quitar comillas a algun texto
+            palabraBuscada = re.findall(patron,comando)   #aqui se almacena la palabra que queremos buscar
+
+            contador3 = 0
+            while contador3 < len(listaArchivos):
+                archivoJSON = listaArchivos[contador3] #recorriendo los archivos
+                cantidadRegistros = len(archivoJSON)   #numero de registros
+                contador = 0
+
+                while contador < cantidadRegistros:    #recorriendo los registros de cada archivo
+                    registro = archivoJSON[contador]      #en var "registro" estaran los registro de los archivos
+                    if registro["nombre"] == palabraBuscada[0]:   #comparando las palabras
+                        print(registro["nombre"],"     ",registro["edad"],"     ",registro["activo"],"     ",registro["promedio"])
+                        contador= cantidadRegistros + 1
+                        contador3 = len(listaArchivos)     #para que despues no siga buscando cuando ya encontro la coincidencia
+                    contador = contador +1
+                contador3 = contador3 + 1
+        
+        else:
+            print("Ha ocurrido un error")
     if ListaTextoComando[0] == "SELECCIONAR*": 
 
         print("Nombre           Edad       Activo       Promedio")
@@ -37,9 +61,9 @@ while salir==False:
 
             contador = 0
 
-            while contador < cantidadRegistros: 
+            while contador < cantidadRegistros:    #recorriendo los registros de cada archivo
                 registro = archivoJSON[contador]      #en var "registro" estaran los registro de los archivos
-                print(registro["nombre"],'           ',registro["edad"],'       ',registro["activo"],'       ',registro["promedio"])
+                print(registro["nombre"],'        ',registro["edad"],'       ',registro["activo"],'       ',registro["promedio"])
                 contador= contador + 1
             contador2 = contador2 + 1
 
